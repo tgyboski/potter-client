23006a96a678969f98dc5e1b3d421529f660fc45
Thiago Gyboski <tgmengatto@gmail.com>
2025-05-30 18:44:18 -0300

    wip: build window

--- modules/corelib/webview.lua
+++ modules/corelib/webview.lua
@@ -2,12 +2,19 @@
 local currentWebView = nil
 local webviewShowEvent = nil
 
+local function getUrlBase()
+  return "file://" .. g_resources.getWorkDir() .. "/modules/game_build/dist/index.html#/"
+end
+
+disconnect(g_game, { onGameEnd = webviewOnGameEnd })
+
 function openWebView(url)
-  g_logger.info("Abrindo WebView: " .. url)
+  local fullUrl = getUrlBase() .. url
+  g_logger.info("Abrindo WebView: " .. fullUrl)
   
   -- Se já existe uma WebView, apenas atualiza a URL
   if currentWebView then
-    currentWebView:loadUrl(url)
+    currentWebView:loadUrl(fullUrl)
 
     if webviewShowEvent then
       removeEvent(webviewShowEvent)
@@ -21,8 +28,7 @@ function openWebView(url)
   end
   
   -- Cria nova WebView se não existir
-  currentWebView = WebView2Panel.create(url)
-
+  currentWebView = WebView2Panel.create(fullUrl)
   
   -- Registra o callback mantendo a referência
   currentWebView:onMessage("close", webviewCloseCallback)
@@ -33,4 +39,11 @@ end
 webviewCloseCallback = function(parameters)
   g_logger.info("WEBVIEW CLOSE")
   currentWebView:hide()
+end
+
+function webviewOnGameEnd()
+  g_logger.info("WEBVIEW ON GAME END")
+  if currentWebView then
+    currentWebView:hide()
+  end
 end
---
+++ modules/game_build/building_items.lua
@@ -0,0 +1,59 @@
+local tabs = {
+    {
+      label = "Produção",
+      canBuild = true,
+      disabled = false,
+      items = {
+        {
+          id = 1,
+          ingredients = {
+            { id = 5901, amount = 4, hasAmount = false },
+            { id = 953, amount = 2, hasAmount = true }
+          }
+        }
+      }
+    },
+    {
+      label = "Armazenamento",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Comida",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Infraestrutura",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Iluminação",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Fundamentos",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Defesa",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    },
+    {
+      label = "Móveis",
+      canBuild = true,
+      disabled = false,
+      items = {}
+    }
+  }
+  
